9924f8dbd9c3d01bdcf781757a9be2dfa80bcf2c
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-07 18:06:34 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,5 +9,9 @@ player.on('timeupdate', handleTimeupdate);
 function handleTimeupdate(currentTime) {
     console.log(currentTime.seconds);
     localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime.seconds));
+
+    const savedTime = localStorage.getItem("videoplayer-current-time");
+    const parsedTime = JSON.parse(savedTime);
+    console.log(parsedTime);
 }
 

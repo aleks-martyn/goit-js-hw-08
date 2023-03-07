97b63832329794f414b3dbb49b05f4ce9624a700
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-07 17:50:10 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+const formEl = document.querySelector('.feedback-form');
 
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+formEl.addEventListener('input', handleFormElInput);
+
+formEl.addEventListener('submit', handleFormElSubmit);
+
+function handleFormElInput(event) {
+    const {
+    elements: { email, message }
+    } = event.currentTarget;
+    
+    const userFeedback = {
+        email: email.value,
+        message: message.value,
+    };
+
+    localStorage.setItem("userFeedback", JSON.stringify(userFeedback));
+
+    const savedSettings = localStorage.getItem("userFeedback");
+    const parsedSettings = JSON.parse(savedSettings);
+    console.log(parsedSettings);
+}
+
+function handleFormElSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+}

e55734d3e39b410cde9c6d15b5543738796dc94b
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-07 17:50:02 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,3 +4,10 @@ const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 
+player.on('timeupdate', handleTimeupdate);
+
+function handleTimeupdate(currentTime) {
+    console.log(currentTime.seconds);
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime.seconds));
+}
+

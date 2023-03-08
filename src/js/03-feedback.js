be57caca5e4478ef7366bd003e09987b12b66747
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-08 16:39:50 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,30 +2,50 @@ import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
 
-const LOCALSTORAGE_KEY = "feedback-form-state";
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
-formEl.addEventListener('input', handleFormElInput);
+const email = formEl.elements.email;
+
+const message = formEl.elements.message;
+
+UpdatePage();
+
+const throttledHandleFormElInput = throttle(handleFormElInput, 500);
+
+formEl.addEventListener('input', throttledHandleFormElInput);
 
 formEl.addEventListener('submit', handleFormElSubmit);
 
+
+
 function handleFormElInput(event) {
-    const {
-    elements: { email, message }
-    } = event.currentTarget;
-    
-    const userFeedback = {
-        email: email.value,
-        message: message.value,
-    };
+  const userFeedback = {
+    email: email.value,
+    message: message.value,
+  };
 
-    localStorage.setItem("userFeedback", JSON.stringify(userFeedback));
-
-    const savedSettings = localStorage.getItem("userFeedback");
-    const parsedSettings = JSON.parse(savedSettings);
-    console.log(parsedSettings);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userFeedback));
 }
 
 function handleFormElSubmit(event) {
-    event.preventDefault();
-    event.currentTarget.reset();
+  event.preventDefault();
+
+  if (email.value === '' || message.value === '') {
+    return console.log('Please fill in all the fields!');
+  }
+
+  const savedSettings = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parsedSettings = JSON.parse(savedSettings);
+  console.log(parsedSettings);
+
+  localStorage.clear();
+
+  event.currentTarget.reset();
+}
+
+function UpdatePage() {
+  const savedSettings = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parsedSettings = JSON.parse(savedSettings);
+  email.value = parsedSettings || "";
+  message.value = parsedSettings || "";
 }
